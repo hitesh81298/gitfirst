325ad97c86b6cabd851ccc24ba016026767f4b03
hitesh kuamr <64910118+hitesh81298@users.noreply.github.com>
2024-05-23 20:52:39 +0530

    all files v 1.0

---
+++ javascript.js
@@ -0,0 +1,127 @@
+// for (i=1;i<=100;i++){
+//     console.log(i);
+// }
+// let a =prompt('enter value of a')
+// console.log(a)
+
+// const hi = ()=>{
+//     for (i=1;i<=100;i++){
+//      console.log(i);
+//     }
+// }
+// hi()
+
+// obj ={   
+    // harry:85,
+    // hk:95,
+    // rk:20
+// }
+// for (let v of obj){
+//     console.log(v)
+// }
+
+// for (a in obj){
+//     console.log("Marks of " + a +" is "+ obj[a]);
+// }
+
+// let cn=88
+// let i
+//let i=prompt('enter a number')
+// while (i != cn) {
+//     i=prompt('enter a number')
+//     console.log('try again');
+
+// }
+// do {
+//     i=prompt('enter a number')
+//     console.log('try again');
+// } while (i != cn);
+// console.log('you enterd correct number');
+
+// let str="hello world! it's dark"
+// for (let i=0;i<str.length;i++){
+//     console.log(str[i]);
+// }
+// for (const i in str){
+//     console.log(str[i])     
+// }
+// console.log(str.slice(3,8));
+// console.log(str.slice(3));
+
+// let hii="dark"
+// console.log("this sentence" + str.includes(hii)? "is":"is not" + "include ${hii}");
+// console.log(`this sentence ${str.includes(hii)? "is":"is not"} include ${hii}`)
+// let upp=str.toUpperCase();
+// console.log(upp);
+// console.log(upp.toLowerCase());
+
+// let ar=['h','a','e','c','r','s']
+// console.log(ar.sort())
+// ar.forEach( b=>{console.log(b);})
+
+// let narr=Array.from(str)
+// for (const i of narr) {
+//     console.log(i);
+// }
+// for (const b in narr) {
+//     console.log(b);
+// }
+
+// let x=[5,6,4,6,12,46]
+// x.push(prompt('enter nummber'))
+// console.log(x);
+// let z=x.map((d)=>{
+//     return d*d;
+// })
+// console.log(z);
+
+// let nu=Math.random()*100;
+// let num=Math.round(nu)
+// // console.log(num)
+//     for(let j=1; j<=100;j++){
+//         let i=prompt('guess the number');
+//         if(i==num){
+//             console.log('you enter correct number in ',j,"attempt");
+//             break
+//         }
+//         else if(i<num){
+//             console.log('the correct number is greater than',i);
+//         }
+//         else if (i>num){ 
+//             console.log('the correct number is less than',i);
+//         }
+//     }    
+// document.footer.style.background = "red"
+
+// let x = prompt("Enter Your age : ")
+// x = Number.parseInt(x)
+// if(x<18){
+//     alert("you can not drive you are under age")
+// }
+// else{
+//     console.log('you can drive');
+// }
+// 
+// let nav=document.getElementsByTagName("li")[0]
+// nav.firstElementChild.style.color= "red"
+// nav.firstElementChild.firstElementChild.firstElementChild.style.color="blue"
+// console.log(nav.firstElementChild.firstElementChild.style.color);
+// let a=document.getElementsByTagName('body')
+// console.log(a);
+// a.classlist.add('b1')
+
+// let cl=document.getElementById('header')
+// let he=document.getElementById('he')
+// setInterval(() => {
+//     he.classList.toggle('b1');
+// }, 2000);
+// console.log(d);
+
+// setInterval(() => {
+// const d= new Date();
+// let h= d.getHours();
+// let m= d.getMinutes();
+// let s= d.getSeconds();
+    // document.getElementById('dat').innerHTML= "Time is : "+h+" : "+m+" : "+s;
+    // }, 1000);
+
